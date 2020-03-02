be0818e9ca1cb72d8ed3069a4a8b75673fb970ff
Simon <design@simonwoodard.com.au>
2020-03-03 10:14:47 +1100

    Initial commit (#2)
    * Initial commit
    
    Contains Vodafone SMS Service Provider for Laravel Notifications Repository
    
    * Update src/VodafoneChannel.php
    
    DocBlock to contain correct class reference
    
    Co-Authored-By: atymic <atymicq@gmail.com>
    
    * Update src/VodafoneClient.php
    
    Include secondary backup in case of failed notification response
    
    Co-Authored-By: atymic <atymicq@gmail.com>
    
    * Update CouldNotSendNotification.php
    
    Adjust backticks to single quotations
    
    * Update VodafoneChannel.php
    
    Style updates
    
    * Update VodafoneClient.php
    
    Code style updates
    
    * Update VodafoneMessage.php
    
    Code style updates
    
    * Update VodafoneChannel.php
    
    Code style update
    
    Co-authored-by: atymic <atymicq@gmail.com>

---
+++ src/VodafoneClient.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace NotificationChannels\Vodafone;
+
+use GuzzleHttp\Client;
+use NotificationChannels\Vodafone\Exceptions\CouldNotSendNotification;
+
+class VodafoneClient
+{
+    /**
+     * @var string Vodafone's API endpoint
+     */
+    protected $endpoint = 'https://www.smsertech.com/apisend';
+
+    /** @var string Vodafone SMS username */
+    protected $username;
+
+    /** @var string Vodafone SMS password */
+    protected $password;
+
+    /**
+     * VodafoneClient constructor.
+     *
+     * @param $username
+     * @param $password
+     */
+    public function __construct()
+    {
+        $this->username = config('services.vodafone.username');
+        $this->password = config('services.vodafone.password');
+
+        return $this;
+    }
+
+    /**
+     * @param $from
+     * @param $to
+     * @param $message
+     *
+     * @return mixed Vodafone API result
+     */
+    public function send($from, $to, $message)
+    {
+        $client = new Client();
+        $res = $client->post($this->endpoint, [
+            'form_params' => [
+                'username'  => $this->username,
+                'password'  => $this->password,
+                'to'        => $to,
+                'message'   => $message,
+                'from'      => $from,
+                'format'    => 'json',
+                'flash'     => 0,
+            ],
+        ]);
+
+        if (!$res) {
+            throw CouldNotSendNotification::serviceUnknownResponse();
+        }
+
+        $body = json_decode($res->getBody()->getContents())[0] ?? null;
+
+        if ($body->status === 'ERROR') {
+            throw CouldNotSendNotification::serviceRespondedWithAnError($body);
+        }
+
+        return $body;
+    }
+}
